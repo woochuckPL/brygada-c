2dda14545df6904a02f7e1180f42012eac2af2a7
woochuckPL <70722164+woochuckPL@users.noreply.github.com>
2021-09-28 11:18:46 +0200

    Adding Harmonogram Activity.

---
+++ app/src/main/java/pl/woochuck/brygadac/harmonogram/HarmonogramActivity.java
@@ -0,0 +1,15 @@
+package pl.woochuck.brygadac.harmonogram;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pl.woochuck.brygadac.R;
+
+public class HarmonogramActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_harmonogram);
+    }
+}
