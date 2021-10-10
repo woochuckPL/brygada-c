6d507260c931989f7aa466cd25d0b4991e0afeee
woochuckPL <70722164+woochuckPL@users.noreply.github.com>
2021-10-10 14:30:06 +0200

    Adding logic for filling RecyclerView with data from Database and parsing data from external SQL database REST API.

--- app/src/main/java/pl/woochuck/brygadac/harmonogram/HarmonogramActivity.java
+++ app/src/main/java/pl/woochuck/brygadac/harmonogram/HarmonogramActivity.java
@@ -1,15 +1,125 @@
 package pl.woochuck.brygadac.harmonogram;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 import pl.woochuck.brygadac.R;
 
 public class HarmonogramActivity extends AppCompatActivity {
+
+    private DayDAO dayDAO;
+    private List<Day> days;
+    private String jsonList;
+
+    private DayAdapter dayAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_harmonogram);
+
+        recyclerView = (RecyclerView) findViewById(R.id.dayRecycler);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        dayDAO = new DayDAO(this);
+
+        fillRecyclerView();
+    }
+
+    public void onClickBtnUpdate (View view) {
+        ConnectionRunnable connectionRunnable = new ConnectionRunnable();
+
+        Thread thread = new Thread(connectionRunnable);
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        jsonList = connectionRunnable.getJsonList();
+        parseJsonToList(jsonList);
+
+        for (Day day : days) {
+            dayDAO.updateById(day);
+        }
+
+        fillRecyclerView();
+    }
+
+    private void parseJsonToList (String jsonList) {
+        Gson gson = new Gson();
+        Type listOfDays = new TypeToken<ArrayList<Day>>() {}.getType();
+        days = gson.fromJson(jsonList, listOfDays);
+    }
+
+    private void fillRecyclerView() {
+        List<Day> daysFromDb = dayDAO.findAll();
+        dayAdapter = new DayAdapter(daysFromDb);
+        recyclerView.setAdapter(dayAdapter);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    class ConnectionRunnable implements Runnable {
+
+        String jsonList;
+
+        public String getJsonList() {
+            return jsonList;
+        }
+
+        @Override
+        public synchronized void run() {
+            try {
+                Properties properties = new Properties();
+                AssetManager assetManager = getApplicationContext().getAssets();
+                properties.load(assetManager.open("config.properties"));
+                String harmonogramUrl = (String) properties.get("harmonogramUrl");
+                URL url = new URL(harmonogramUrl );
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoInput(true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                jsonList = response.toString();
+
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+////                        textViewTest.append(jsonList);
+//                    }
+//                });
+            } catch (IOException e) {
+                Log.e("ErrorIO", e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 }
